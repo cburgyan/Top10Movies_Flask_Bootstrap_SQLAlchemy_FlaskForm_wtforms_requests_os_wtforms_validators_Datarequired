2e1f420c7695eff592bd46bc9ed63e8b178b91a7
cburgyan <cburgyan@gmail.com>
2022-08-05 16:49:46 -0500

    add and integrate sorting and ranking functions

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+import os
+
+
+API_KEY = os.environ.get('TMDB_KEY')
+TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500"
+TMDB_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie'
+search_movie_list = []
 
 
 # Create Flask Object
@@ -38,11 +45,14 @@ class RateMovieForm(FlaskForm):
     review = StringField('Your Review', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+class AddMovieForm(FlaskForm):
+    title = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
+
+
 # Create Table
 db.create_all()
-
-
-
 
 
 # new_movie = Movie(
@@ -73,9 +83,64 @@ db.create_all()
 # db.session.commit()
 
 
+def merge(movies_list, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0] * n1
+    R = [0] * n2
+
+    for i in range(0, n1):
+        L[i] = movies_list[l + i]
+
+    for j in range(0, n2):
+        R[j] = movies_list[m + 1 + j]
+
+    i = 0
+    j = 0
+    k = l
+
+    while i < n1 and j < n2:
+        if L[i].rating >= R[j].rating:
+            movies_list[k] = L[i]
+            i += 1
+        else:
+            movies_list[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        movies_list[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        movies_list[k] = R[j]
+        j += 1
+        k += 2
+
+
+def mergesort(movies_list, l, r):
+    if l < r:
+        m = l + (r-l)//2
+
+        mergesort(movies_list, l, m)
+        mergesort(movies_list, m + 1, r)
+        merge(movies_list, l, m, r)
+
+
+def rank_ordered_movies(movies_list, length):
+    for index in range(0, length):
+        movies_list[index].ranking = index + 1
+
+
 @app.route("/")
 def home():
-    return render_template("index.html", movies=Movie.query.all())
+    movie_list = Movie.query.all()
+    num_of_movies = len(movie_list)
+    mergesort(movie_list, 0, num_of_movies - 1)
+    rank_ordered_movies(movie_list, num_of_movies)
+    return render_template("index.html", movies=movie_list)
 
 
 @app.route("/edit/<id>", methods=['POST', 'GET'])
@@ -97,6 +162,52 @@ def delete_movie(id):
     db.session.commit()
     return redirect(url_for('home'))
 
+
+@app.route('/add', methods=['POST', 'GET'])
+def add_movie():
+    global search_movie_list
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        parameters = {
+            "api_key": API_KEY,
+            "query": title,
+            "page": '1',
+            "include_adult": 'false'
+        }
+
+        response = requests.get(TMDB_SEARCH_URL, params=parameters)
+        search_movie_list = response.json()['results']
+        return render_template('select.html', movies_list=search_movie_list)
+
+    return render_template('add.html', form=form)
+
+
+@app.route('/select<id>')
+def select_movie(id):
+    print("HELLO")
+    print(id)
+    movie = {}
+    for movie_element in search_movie_list:
+        print(movie_element['id'])
+        if str(movie_element['id']) == str(id):
+            movie = movie_element
+    print("Movie: ")
+    print(movie)
+    new_movie = Movie(
+        id=id,
+        title=movie['title'],
+        year=movie['release_date'][:4],
+        description=movie['overview'],
+        rating=0.0,
+        ranking=0,
+        review="",
+        img_url=f'{TMDB_IMG_URL}{movie["poster_path"]}'
+    )
+
+    db.session.add(new_movie)
+    db.session.commit()
+    return redirect(url_for('edit_page', id=id))
 
 if __name__ == '__main__':
     app.run(debug=True)
